36078602bad17d9dac2fb069e7f2f8d1080a2d94
rv2hex <124544953+rv2hex@users.noreply.github.com>
2023-03-06 21:35:21 +0530

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,11 @@
+#/bin/bash/
+
+a = input("What is your ?")
+b = input("What your date of birth ?")
+c = input("What gender are you ?")
+d = input("career interest ?")
+
+print( "name             :" , a)
+print( "DOB dd/mm/yyyy   :" , b)
+print( "gneder           :" , c)
+print( "interset         :" , d)
